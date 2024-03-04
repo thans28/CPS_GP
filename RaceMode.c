006092da18ef11111f91afcf1d1f196de61b4698
Tyler Hansen <122710485+thans28@users.noreply.github.com>
2024-03-04 18:26:55 -0500

    Added motors & PWM

--- RaceMode.c
+++ RaceMode.c
@@ -19,9 +19,34 @@ void RaceMode_Init(void){
     P7->SEL1 &=~0xFF;
     P7->DIR &=~0xFF;
 
-
-
     Clock_Init48MHz();
+
+    // Motors Init
+    P5->SEL0 &=~0x30;
+    P5->SEL1 &=~0x30;
+    P2->SEL0 &=~0xC0;
+    P2->SEL1 &=~0xC0;
+    P3->SEL0 &=~0xC0;
+    P3->SEL1 &=~0xC0;
+    P5->DIR |= 0x30;
+    P2->DIR |= 0xC0;
+    P3->DIR |= 0xC0;
+    P3->OUT |= 0xC0;
+
+    // PWM Init --> period = 15000, duty3 = 0, duty4 = 0
+    if(duty3 >= 15000) return; // bad input
+    if(duty4 >= 15000) return; // bad input
+    P2->DIR |= 0xC0;          // P2.6, P2.7 output
+    P2->SEL0 |= 0xC0;         // P2.6, P2.7 Timer0A functions
+    P2->SEL1 &= ~0xC0;        // P2.6, P2.7 Timer0A functions
+    TIMER_A0->CCTL[0] = 0x0080;      // CCI0 toggle
+    TIMER_A0->CCR[0] = 15000;       // Period is 2*period*8*83.33ns is 1.333*period
+    TIMER_A0->EX0 = 0x0000;        //    divide by 1
+    TIMER_A0->CCTL[3] = 0x0040;      // CCR3 toggle/reset
+    TIMER_A0->CCR[3] = 0;        // CCR3 duty cycle is duty3/period
+    TIMER_A0->CCTL[4] = 0x0040;      // CCR4 toggle/reset
+    TIMER_A0->CCR[4] = 0;        // CCR4 duty cycle is duty4/period
+    TIMER_A0->CTL = 0x02F0;        // SMCLK=12MHz, divide by 8, up-down mode
 }
 
 //read in 8-bit input from line sensors
@@ -51,7 +76,7 @@ uint8_t Reflectance_Read(void){
 }
 
 //interpret line sensor reading into usable data
-uint8_t InterpretVal(uint8_t data){
+uint8_t InterpretVal(void){
     uint8_t lineSense = Reflectance_Read();
     uint8_t output;
 
@@ -88,6 +113,73 @@ uint8_t InterpretVal(uint8_t data){
 
     return output;
 }
+
+
+void PWM_Duty3(uint16_t duty3){
+    if(duty3 >= TIMER_A0->CCR[0]) return; // bad input
+      TIMER_A0->CCR[3] = duty3;        // CCR3 duty cycle is duty3/period
+
+}
+
+
+void PWM_Duty4(uint16_t duty4){
+    if(duty4 >= TIMER_A0->CCR[0]) return; // bad input
+      TIMER_A0->CCR[4] = duty4;        // CCR4 duty cycle is duty4/period
+
+}
+
+
+void Motor_Stop(void){
+    P5->OUT &=~0x30;
+    P2->OUT &=~0xC0;
+    P3->OUT &=~0xC0;
+}
+
+
+void Motor_Forward(uint16_t leftDuty, uint16_t rightDuty){
+  // write this as part of Lab 13
+    P5->OUT &=~0x30;
+    P2->OUT |= 0xC0;
+    P3->OUT |= 0xC0;
+    PWM_Duty3(rightDuty);
+    PWM_Duty4(leftDuty);
+}
+
+void Motor_Right(uint16_t leftDuty, uint16_t rightDuty){
+  // write this as part of Lab 13
+    P5->OUT &=~0x10;
+    P2->OUT |= 0x80;
+    P3->OUT |= 0x80;
+    P5->OUT |= 0x20;
+    P2->OUT |= 0x40;
+    P3->OUT |= 0x40;
+    PWM_Duty3(rightDuty);
+    PWM_Duty4(leftDuty);
+}
+
+
+void Motor_Left(uint16_t leftDuty, uint16_t rightDuty){
+  // write this as part of Lab 13
+    P5->OUT &=~0x20;
+    P2->OUT |= 0x40;
+    P3->OUT |= 0x40;
+    P5->OUT |= 0x10;
+    P2->OUT |= 0x80;
+    P3->OUT |= 0x80;
+    PWM_Duty3(rightDuty);
+    PWM_Duty4(leftDuty);
+}
+
+
+void Motor_Backward(uint16_t leftDuty, uint16_t rightDuty){
+  // write this as part of Lab 13
+    P5->OUT |= 0x30;
+    P2->OUT |= 0xC0;
+    P3->OUT |= 0xC0;
+    PWM_Duty3(rightDuty);
+    PWM_Duty4(leftDuty);
+}
+
 
 //motor control
 void MotorsRun(uint32_t left, uint32_t right, uint8_t dir, uint32_t time){
