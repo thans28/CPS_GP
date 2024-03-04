a254872be5168f23b790f37acb642e58808b42b5
Tyler Hansen <122710485+thans28@users.noreply.github.com>
2024-03-04 18:27:37 -0500

    Added PWM & Motors

--- RaceMode.h
+++ RaceMode.h
@@ -11,6 +11,20 @@ uint8_t Reflectance_Read(void);
 //interpret line sensor reading into usable data
 uint8_t InterpretVal(void);
 
+void PWM_Duty3(uint16_t duty3);
+
+void PWM_Duty4(uint16_t duty4);
+
+void Motor_Stop(void);
+
+void Motor_Forward(uint16_t leftDuty, uint16_t rightDuty);
+
+void Motor_Right(uint16_t leftDuty, uint16_t rightDuty);
+
+void Motor_Left(uint16_t leftDuty, uint16_t rightDuty);
+
+void Motor_Backward(uint16_t leftDuty, uint16_t rightDuty);
+
 //motor control
 void MotorsRun(uint32_t left, uint32_t right, uint8_t dir, uint32_t time);
 
