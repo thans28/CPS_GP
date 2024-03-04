8788d232fd40dd1303ddfe89c972b89e0019e29a
Tyler Hansen <122710485+thans28@users.noreply.github.com>
2024-03-04 11:11:35 -0500

    Added interpreter function
    Also adjusted FSM table to include default that does not change state of machine for when the line sensors read in something weird.

--- RaceMode.c
+++ RaceMode.c
@@ -30,7 +30,7 @@ uint8_t Reflectance_Read(void){
 
     P4->SEL0 &= ~0xFF;
     P4->SEL1 &= ~0xFF;    //  P4.0 as GPIO
-    P4->DIR |= 0xFF; 
+    P4->DIR |= 0xFF;
 
     P5->OUT |= 0x08;      // turn on 4 even IR LEDs
     P9->OUT |= 0x04;
@@ -52,7 +52,41 @@ uint8_t Reflectance_Read(void){
 
 //interpret line sensor reading into usable data
 uint8_t InterpretVal(uint8_t data){
-    
+    uint8_t lineSense = Reflectance_Read();
+    uint8_t output;
+
+    switch(lineSense){
+    case (0x00 || 0xFF): //none or all sensors
+        output = 0x01;
+        break;
+    case 0x18: //center 2 sensors
+        output = 0x02;
+        break;
+    case 0xF0: //left 4 sensors
+        output = 0x03;
+        break;
+    case 0x0F: //right 4 sensors
+        output = 0x04;
+        break;
+    case (0xC0 || 0x60): //two of the left sensors
+        output = 0x05;
+        break;
+    case (0x06 || 0x03): //two of the right sensors
+        output = 0x06;
+        break;
+    case 0x30: //two sensors just to the left of center
+        output = 0x07;
+        break;
+    case 0xC0: //two sensors just to the right of center
+        output = 0x08;
+        break;
+    default: // if we get something wonky, just ignore it and stay in the same state
+        output = 0x00;
+        break;
+    }
+
+
+    return output;
 }
 
 //motor control
